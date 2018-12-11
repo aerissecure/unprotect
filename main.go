6d46dcec542ba686c74876cfcdb64ae19bc62dfa
Jeffrey Stiles <freb@users.noreply.github.com>
2018-12-11 10:10:51 -0800

    initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"strings"
+
+	"baliance.com/gooxml/document"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: unprotect <file1,[file2],...>")
+		os.Exit(1)
+	}
+	for _, file := range os.Args[1:] {
+		doc, err := document.Open(file)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		doc.Settings.X().DocumentProtection = nil
+		filename := path.Base(file)
+		ext := path.Ext(filename)
+		trunk := strings.TrimSuffix(filename, ext)
+		doc.SaveToFile(trunk + "-unlocked" + ext)
+	}
+}
