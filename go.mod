a160ad10c73b2dc0ae2c55d44a3eec7056d2e251
Jeffrey Stiles <freb@users.noreply.github.com>
2020-08-06 09:21:43 -0700

    upgrade to unioffice, add license, add go.mod, fix print statements

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/aerissecure/unprotect
+
+go 1.14
+
+require github.com/unidoc/unioffice v1.3.0
--- main.go
+++ main.go
@@ -6,18 +6,27 @@ import (
 	"path"
 	"strings"
 
-	"baliance.com/gooxml/document"
+	"github.com/unidoc/unioffice"
+	"github.com/unidoc/unioffice/common/license"
+	"github.com/unidoc/unioffice/document"
 )
 
 func main() {
+	if err := license.SetLegacyLicenseKey(unidocLicense); err != nil {
+		fmt.Println("error installing unioffice license:", err)
+		os.Exit(1)
+	}
+
+	unioffice.DisableLogging()
+
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: unprotect <file1,[file2],...>")
+		fmt.Fprintln(os.Stderr, "Usage: unprotect <file1,[file2],...>")
 		os.Exit(1)
 	}
 	for _, file := range os.Args[1:] {
 		doc, err := document.Open(file)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			fmt.Fprintf(os.Stderr, "error opening %s: %v\n", file, err)
 			continue
 		}
 		doc.Settings.X().DocumentProtection = nil
@@ -25,5 +34,6 @@ func main() {
 		ext := path.Ext(filename)
 		trunk := strings.TrimSuffix(filename, ext)
 		doc.SaveToFile(trunk + "-unprotected" + ext)
+		fmt.Println(trunk + "-unprotected" + ext)
 	}
 }
